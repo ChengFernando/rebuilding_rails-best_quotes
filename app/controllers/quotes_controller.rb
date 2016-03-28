97206173ecd62234d48dd522186669348ed2cf7e
ChengFernando <fernandocheng2010@gmail.com>
2016-03-28 10:36:31 +0800

    CH5 Basic Models - 2

--- app/controllers/quotes_controller.rb
+++ app/controllers/quotes_controller.rb
@@ -12,5 +12,22 @@ class QuotesController < Rulers::Controller
     render :quote, :obj => quote_1
   end
 
+  def index
+    quotes = FileModel.all
+    render :index, :quotes => quotes
+  end
+
+  def new_quote
+    attrs = {
+        "submitter" => "web user",
+        "quote" => "A picture is worth one k pixels",
+        "attribution" => "me"
+      }
+
+    m = FileModel.create attrs
+    render :quote, :obj => m
+  end
+
+
 end
 # test on "rerun" gem 
